2ba44fe00ecd131d029ca33aa141b92bbb23bdaf
Ivan Zavyalov <laceupalive@gmail.com>
2019-11-30 08:22:48 +0300

    GPIO fix.

--- pi.py
+++ pi.py
@@ -3,17 +3,20 @@ import json
 
 import gpiozero
 from gpiozero import LEDBoard
+from time import sleep
 
 def light(data, enable):
-    led = LEDBoard(data)
+    led = LEDBoard(*data)
     if enable == 1: 
         led.on()
     else: 
         led.off()
-    sleep(0.1)
+    sleep(0.038)
 
 if __name__ == "__main__":
     dataOn = json.loads(sys.argv[1])
     dataOff = json.loads(sys.argv[2])
-    light(dataOn, 1)
-    light(dataOff, 0)
+    if len(dataOn) > 0:
+        light(dataOn, 1)
+    if len(dataOff) > 0
+        light(dataOff, 0)
